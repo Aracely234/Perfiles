ba9d63e645d3e5ed3e172b2ea1a1e089de9974d6
Aracely234 <aracely08t.n@gmail.com>
2023-02-17 18:36:24 -0800

    Comentarios actualizados

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /*
-PokeApi
-Implementación de una Api (pokeapi) con JS
+Perfil
+Implementación de una Api (Perfiles) con JS
 
 Integrantes:
-De la Cruz Yamahoka Daniela 19211624
-García Arellano Aracely 19211642
+García Arellano Aracely #19211642
+González Guzman María José #19211650
 */
 
 //Se traen los atributos del html a una constante para utilizarlos
@@ -12,7 +12,7 @@ García Arellano Aracely 19211642
 const button = document.querySelector('button');
 const pokemonContainer = document.querySelector('.card'); //es un tipo de atributo que deriva del css
 
-//Al botón se le agrega un evento para usar la función traerPokemon
+//Al botón se le agrega un evento para usar la función traerperfil
 button.addEventListener('click', (e)=>{
     e.preventDefault();
     traerPerfil();
@@ -32,10 +32,10 @@ function hide_the_Loading(){
     card.style.display = 'block';
 }
 
-//Función para traer los datos de los pokemones
+//Función para traer los datos de los perfiles
 function traerPerfil(){ //se le agrega un parámetro
     displayLoading();//Se llama al cargador
-    fetch(`https://randomuser.me/api/?results=5000`)//se busca la url con el parametro del pokemon
+    fetch(`https://randomuser.me/api/?results=5000`)//se busca la url del perfil
     .then((res) => res.json())//se convierten los datos a un archivo json
     .then((data)=> { //se pone en una variable para utilizar los datos
         hide_the_Loading();//Cuando se obtienen los datos se llama al cerrar cargador
@@ -45,7 +45,7 @@ function traerPerfil(){ //se le agrega un parámetro
     
 }
 
-//Función para agregar los datos de los pokemones
+//Función para agregar los datos de los perfiles
 
 function crearPerfil(data){
     for(var i=0; i <5000; i++){
@@ -56,23 +56,23 @@ function crearPerfil(data){
         const nombre= document.getElementById('nombre');
         nombre.textContent=data.results[i].name.first +" "+ data.results[i].name.last;
 
-        //Se crea una constante para el tipo de pokemon y se busca en los datos.
+        //Se crea una constante para el email de la persona y se busca en los datos.
         const email= document.getElementById('email');
         email.textContent=data.results[i].email;
 
-        //Se crea una constante para la experiencia y se busca en los datos.
+        //Se crea una constante para la nacionalidad y se busca en los datos.
         const nat= document.getElementById('nat');
         nat.textContent=data.results[i].nat;
 
-        //Se crea una constante para la defensa y se busca en los datos.
+        //Se crea una constante para la direccion y se busca en los datos.
         const adress = document.getElementById('adrees');
         adress.textContent= "ADRESS: "+data.results[i].location.street.number +" "+data.results[i].location.street.name;
 
-        //Se crea una constante para el ataque y se busca en los datos.
+        //Se crea una constante para el numero de celular y se busca en los datos.
         const cell = document.getElementById('cell');
         cell.textContent= data.results[i].cell;
 
-        //Se crea una constante para llos puntos de vida y se busca en los datos.
+        //Se crea una constante para los años y se busca en los datos.
         const age = document.getElementById('age');
         age.textContent= data.results[i].dob.age;
     }
