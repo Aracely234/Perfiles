f1d253f4c3765bc766586500d8096c7382a377a7
Maria Jose Gonzalez Guzman <mguzman@bapcorporativo.com>
2023-02-17 18:14:39 -0800

    first commit

--- index.js
+++ index.js
@@ -8,21 +8,37 @@ García Arellano Aracely 19211642
 */
 
 //Se traen los atributos del html a una constante para utilizarlos
-const input = document.querySelector('input');
+// const input = document.querySelector('input');
 const button = document.querySelector('button');
 const pokemonContainer = document.querySelector('.card'); //es un tipo de atributo que deriva del css
 
 //Al botón se le agrega un evento para usar la función traerPokemon
 button.addEventListener('click', (e)=>{
     e.preventDefault();
-    traerPerfil(input.value);
+    traerPerfil();
 })
+//Funcion para mostrar el cargador
+function displayLoading(){
+    var loder = document.querySelector('#loader');//Guardar el elemento html en una variable
+    loder.style.display = 'block';//Cambiar el display a block para mostrarlo
+    var card = document.querySelector('#card');//Guardar el elemento html en una variable
+    card.style.display = 'none';//Cambiar el display a none para ocultarlo
+}
+//Funcion para mostrar el cargador
+function hide_the_Loading(){
+    var loder = document.querySelector('#loader');
+    loder.style.display = 'none';
+    var card = document.querySelector('#card');
+    card.style.display = 'block';
+}
 
 //Función para traer los datos de los pokemones
-function traerPerfil(name){ //se le agrega un parámetro
+function traerPerfil(){ //se le agrega un parámetro
+    displayLoading();//Se llama al cargador
     fetch(`https://randomuser.me/api/?results=5000`)//se busca la url con el parametro del pokemon
     .then((res) => res.json())//se convierten los datos a un archivo json
     .then((data)=> { //se pone en una variable para utilizar los datos
+        hide_the_Loading();//Cuando se obtienen los datos se llama al cerrar cargador
         crearPerfil(data);//se utiliza la función con los datos para elegir qué elementos usar
         console.log(data);//se imprimen los datos en la consola
     });
@@ -32,7 +48,7 @@ function traerPerfil(name){ //se le agrega un parámetro
 //Función para agregar los datos de los pokemones
 
 function crearPerfil(data){
-    for(const i=0; i <5000; i++){
+    for(var i=0; i <5000; i++){
         const img = document.getElementById('imagenpok');//se crea una constante para otorgar la imagen
         img.src=data.results[i].picture.large;//se busca la imagen en los datos del perfil
 
